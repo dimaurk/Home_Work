b8d5c48677d4eff892fc55319678968fb7b0a2a8
Dmitry Yurkov <dimaurkov1998@mail.ru>
2019-03-23 19:44:55 +0300

    comment

---
+++ HW_4/Task_01/DiffetertsWords/DiffetertsWords/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffetertsWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Text._Str;
+            Text.ReadText(str);
+
+            Console.WriteLine("Количество повторений слов:\n");
+            Text.SortText(str);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW_4/Task_01/DiffetertsWords/DiffetertsWords/Text.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffetertsWords
+{
+    class Text
+    {
+        private static string _str = "Hi, how are you. Hi, i am fine thanks, and how are you. \n";
+
+        public static string _Str
+        {
+            get
+            {
+                return _str;
+            }
+
+            set
+            {
+                _str = value;
+            }
+        }
+
+        public Text(string str)
+        {
+            _Str = str;
+        }
+
+        public static void ReadText(string str)
+        {
+            _Str = str;
+            Console.WriteLine(str);
+        }
+
+        public static void SortText(string str)
+        {
+            _Str = str;
+
+            string[] array = str.Split(new char[] { '\n', '\r', ',', ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            var sort = array.GroupBy(n => n.ToLower());
+
+            foreach (var s in sort)
+            {
+                Console.WriteLine("Слово - {0} [количесво повторений: {1}]", s.Key, s.Count());
+            }
+        }
+    }
+}
