add4ab7bdfa4f5f2c6001723ff86862fc29f856f
Dmitry Yurkov <dimaurkov1998@mail.ru>
2019-03-25 23:24:53 +0300

    comment

---
+++ HW_4/Task_03/Compression/Compression/Compression.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Compression
+{
+    class Compression
+    {
+
+        public static string _str = string.Empty;
+
+        public void GetCompression(string str)
+        {
+            _str = str;
+
+            using (StreamReader reader = new StreamReader(@"goncharov_ivan-obyknovennaja_istorija.txt", Encoding.Default))
+            {
+                str = reader.ReadToEnd();
+            }
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"pack.txt"))
+            {
+                file.Write((new Regex("Гончаров")).Replace(str, "010"));
+            }
+        }
+        public void GetDecompression(string str)
+        {
+            _str = str;
+
+            using (StreamReader reader = new StreamReader(@"pack.txt"))
+            {
+                str = reader.ReadToEnd();
+            }
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"unpack.txt"))
+            {
+                file.Write((new Regex("010")).Replace(str, "Гончаров"));
+            }
+        }
+    }
+}
---
+++ HW_4/Task_03/Compression/Compression/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Compression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compression first = new Compression();
+            string str1 = Compression._str;
+
+            first.GetCompression(str1);
+            first.GetDecompression(str1);
+            Console.WriteLine("Файл 'сжат' и 'восстановлен'");
+            Console.ReadLine();
+        }
+    }
+}
